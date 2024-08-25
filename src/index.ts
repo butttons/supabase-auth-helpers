772629a2fec93cf2b55b4740301d114d6a22934e
Yash <zilllionaire@gmail.com>
2024-08-25 20:48:01 +0530

    Fix cookie parsing; When the session data is within the character limit, it does not get split into multiple cookies it seems. Updated the cookie name checking logic to disregard the period.

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
 import type {
-  Session,
-  UserAppMetadata,
-  UserMetadata,
-} from '@supabase/supabase-js';
-import { type JWTPayload, jwtVerify } from 'jose';
-import { parseCookies } from 'oslo/cookie';
-import debugFactory from 'debug';
+	Session,
+	UserAppMetadata,
+	UserMetadata,
+} from "@supabase/supabase-js";
+import debugFactory from "debug";
+import { type JWTPayload, jwtVerify } from "jose";
+import { parseCookies } from "oslo/cookie";
 
-const debug = debugFactory('@butttons/supabase-auth-helpers');
+const debug = debugFactory("@butttons/supabase-auth-helpers");
 
 /**
  * @module
@@ -19,37 +19,37 @@ const debug = debugFactory('@butttons/supabase-auth-helpers');
  * Options for the `SupabaseAuthHelper` class.
  */
 export type SupabaseAuthHelperOptions = {
-  /**
-   * The Supabase project ID
-   */
-  supabaseId: string;
-  /**
-   * The Supabase project URL. This is used to verify the issuer of the JWT.
-   */
-  supabaseUrl: string;
-  /**
-   * The JWT secret used to sign the JWT.
-   */
-  jwtSecret: string;
+	/**
+	 * The Supabase project ID
+	 */
+	supabaseId: string;
+	/**
+	 * The Supabase project URL. This is used to verify the issuer of the JWT.
+	 */
+	supabaseUrl: string;
+	/**
+	 * The JWT secret used to sign the JWT.
+	 */
+	jwtSecret: string;
 };
 
 /**
  * A simple user object in the JWT payload. This is safer than `getSession().session.user`.
  */
 export type SupabaseTokenUser = {
-  id: string;
-  email: string;
-  phone: string;
-  role: string;
-  is_anonymous: boolean;
-  app_metadata: UserAppMetadata;
-  user_metadata: UserMetadata;
-  session_id: string;
+	id: string;
+	email: string;
+	phone: string;
+	role: string;
+	is_anonymous: boolean;
+	app_metadata: UserAppMetadata;
+	user_metadata: UserMetadata;
+	session_id: string;
 };
 
 type SafeResponse<T> =
-  | { type: 'success'; payload: T }
-  | { type: 'error'; error: unknown };
+	| { type: "success"; payload: T }
+	| { type: "error"; error: unknown };
 
 /**
  * A helper utility to work with Supabase Auth.
@@ -69,126 +69,126 @@ type SafeResponse<T> =
  * ```
  */
 export class SupabaseAuthHelper {
-  public options: SupabaseAuthHelperOptions;
+	public options: SupabaseAuthHelperOptions;
 
-  constructor(options: SupabaseAuthHelperOptions) {
-    this.options = options;
-  }
+	constructor(options: SupabaseAuthHelperOptions) {
+		this.options = options;
+	}
 
-  /**
-   * Decodes the Supabase auth cookie and returns the session object.
-   *
-   * This method is used internally to get the session object from the request.
-   * @param cookies {Map<string, string>}
-   * @returns Session or null if the cookie is not parsable.
-   */
-  private decodeAuthCookie = (cookies: Map<string, string>): Session | null => {
-    const supabaseCookie = Array.from(cookies.entries())
-      .sort(([a, b]) => a[0].localeCompare(b[0]))
-      .reduce((acc, [name, value]) => {
-        if (name.includes(`${this.options.supabaseId}-auth-token.`)) {
-          acc += value;
-        }
-        return acc;
-      }, '');
+	/**
+	 * Decodes the Supabase auth cookie and returns the session object.
+	 *
+	 * This method is used internally to get the session object from the request.
+	 * @param cookies {Map<string, string>}
+	 * @returns Session or null if the cookie is not parsable.
+	 */
+	private decodeAuthCookie = (cookies: Map<string, string>): Session | null => {
+		const supabaseCookie = Array.from(cookies.entries())
+			.sort(([a, b]) => a[0].localeCompare(b[0]))
+			.reduce((acc, [name, value]) => {
+				if (name.includes(`${this.options.supabaseId}-auth-token`)) {
+					acc += value;
+				}
+				return acc;
+			}, "");
 
-    try {
-      return JSON.parse(supabaseCookie) as Session;
-    } catch {
-      debug(`decodeAuthCookie(): Failed to parse cookie: %s.`, supabaseCookie);
-      return null;
-    }
-  };
+		try {
+			return JSON.parse(supabaseCookie) as Session;
+		} catch {
+			debug("decodeAuthCookie(): Failed to parse cookie: %s.", supabaseCookie);
+			return null;
+		}
+	};
 
-  /**
-   * Returns the session object from the request. Similar to `supabase.auth.getSession()`.
-   * The data returned here should be treated as untrusted.
-   *
-   * Use the `getTokenPayload(req)` method to get the validated payload.
-   * @param req
-   * @returns - Supabase Session from `@supabase/supabase-js`
-   */
-  public getUnsafeSession = (req: Request): Session | null => {
-    const cookieHeader = req.headers.get('cookie');
-    if (!cookieHeader) {
-      debug('getUnsafeSession(): Cookies not found');
-      return null;
-    }
+	/**
+	 * Returns the session object from the request. Similar to `supabase.auth.getSession()`.
+	 * The data returned here should be treated as untrusted.
+	 *
+	 * Use the `getTokenPayload(req)` method to get the validated payload.
+	 * @param req
+	 * @returns - Supabase Session from `@supabase/supabase-js`
+	 */
+	public getUnsafeSession = (req: Request): Session | null => {
+		const cookieHeader = req.headers.get("cookie");
+		if (!cookieHeader) {
+			debug("getUnsafeSession(): Cookies not found");
+			return null;
+		}
 
-    const session = this.decodeAuthCookie(parseCookies(cookieHeader));
-    return session;
-  };
+		const session = this.decodeAuthCookie(parseCookies(cookieHeader));
+		return session;
+	};
 
-  /**
-   * Authenticates the token and returns the payload.
-   * Updated the payload to include the `id` field from the `sub` field.
-   * @param token
-   * @returns A minimal user object from the JWT payload.
-   */
-  public authenticateToken = async (
-    token: string,
-  ): Promise<SupabaseTokenUser & JWTPayload> => {
-    const { payload } = await jwtVerify<SupabaseTokenUser>(
-      token,
-      new TextEncoder().encode(this.options.jwtSecret),
-      {
-        issuer: new URL('/auth/v1', this.options.supabaseUrl).toString(),
-      },
-    );
-    payload.id = payload.sub ?? payload.id;
-    return payload;
-  };
+	/**
+	 * Authenticates the token and returns the payload.
+	 * Updated the payload to include the `id` field from the `sub` field.
+	 * @param token
+	 * @returns A minimal user object from the JWT payload.
+	 */
+	public authenticateToken = async (
+		token: string,
+	): Promise<SupabaseTokenUser & JWTPayload> => {
+		const { payload } = await jwtVerify<SupabaseTokenUser>(
+			token,
+			new TextEncoder().encode(this.options.jwtSecret),
+			{
+				issuer: new URL("/auth/v1", this.options.supabaseUrl).toString(),
+			},
+		);
+		payload.id = payload.sub ?? payload.id;
+		return payload;
+	};
 
-  /**
-   * Alternative to `authenticateToken` that returns a `SafeResponse` object, instead of throwing the error.
-   * @param token
-   * @returns A `SafeResponse` object which contains the payload or the error.
-   *
-   * @example
-   *
-   * ```ts
-   * const result = await supabaseHelper.authenticateTokenSafely(token);
-   *
-   * if (result.type === 'error') {
-   *  console.error('Could not verify token', result.error);
-   *  return;
-   * }
-   *
-   * const payload = result.payload;
-   *
-   * ```
-   */
-  public authenticateTokenSafely = async (
-    token: string,
-  ): Promise<SafeResponse<SupabaseTokenUser & JWTPayload>> => {
-    try {
-      const payload = await this.authenticateToken(token);
-      return { type: 'success', payload };
-    } catch (error) {
-      return { type: 'error', error };
-    }
-  };
+	/**
+	 * Alternative to `authenticateToken` that returns a `SafeResponse` object, instead of throwing the error.
+	 * @param token
+	 * @returns A `SafeResponse` object which contains the payload or the error.
+	 *
+	 * @example
+	 *
+	 * ```ts
+	 * const result = await supabaseHelper.authenticateTokenSafely(token);
+	 *
+	 * if (result.type === 'error') {
+	 *  console.error('Could not verify token', result.error);
+	 *  return;
+	 * }
+	 *
+	 * const payload = result.payload;
+	 *
+	 * ```
+	 */
+	public authenticateTokenSafely = async (
+		token: string,
+	): Promise<SafeResponse<SupabaseTokenUser & JWTPayload>> => {
+		try {
+			const payload = await this.authenticateToken(token);
+			return { type: "success", payload };
+		} catch (error) {
+			return { type: "error", error };
+		}
+	};
 
-  /**
-   * Gets the minimal user object from the request.
-   * @param req Request
-   * @returns The token payload or null if the token is invalid.
-   */
-  public getTokenPayload = async (
-    req: Request,
-  ): Promise<(SupabaseTokenUser & JWTPayload) | null> => {
-    const session = this.getUnsafeSession(req);
-    if (!session) {
-      debug('getTokenPayload(): Session not found in the request');
-      return null;
-    }
+	/**
+	 * Gets the minimal user object from the request.
+	 * @param req Request
+	 * @returns The token payload or null if the token is invalid.
+	 */
+	public getTokenPayload = async (
+		req: Request,
+	): Promise<(SupabaseTokenUser & JWTPayload) | null> => {
+		const session = this.getUnsafeSession(req);
+		if (!session) {
+			debug("getTokenPayload(): Session not found in the request");
+			return null;
+		}
 
-    const result = await this.authenticateTokenSafely(session.access_token);
-    if (result.type === 'error') {
-      debug('getTokenPayload(): Failed to parse token - %o', result.error);
-      return null;
-    }
+		const result = await this.authenticateTokenSafely(session.access_token);
+		if (result.type === "error") {
+			debug("getTokenPayload(): Failed to parse token - %o", result.error);
+			return null;
+		}
 
-    return result.payload;
-  };
+		return result.payload;
+	};
 }
